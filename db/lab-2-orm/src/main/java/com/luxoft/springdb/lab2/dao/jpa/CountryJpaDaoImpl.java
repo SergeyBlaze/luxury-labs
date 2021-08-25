d32b464c3c32d8a35b299b14a00708e9714d7670
Kozlov Sergey <s.kozlov@haulmont.com>
2021-08-25 22:50:17 +0400

    lab2-orm

--- db/lab-2-orm/src/main/java/com/luxoft/springdb/lab2/dao/jpa/CountryJpaDaoImpl.java
+++ db/lab-2-orm/src/main/java/com/luxoft/springdb/lab2/dao/jpa/CountryJpaDaoImpl.java
@@ -1,5 +1,6 @@
 package com.luxoft.springdb.lab2.dao.jpa;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -7,24 +8,62 @@ import org.springframework.stereotype.Repository;
 import com.luxoft.springdb.lab2.dao.CountryDao;
 import com.luxoft.springdb.lab2.model.Country;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+
 @Repository
 public class CountryJpaDaoImpl extends AbstractJpaDao implements CountryDao {
 
 	@Override
 	public void save(Country country) {
-		// TODO: Implement it
+
+		EntityManager em = entityManagerFactory.createEntityManager();
+		if (em != null) {
+			EntityTransaction tx = em.getTransaction();
+			tx.begin();
+
+			if (!em.contains(country)) {
+				em.persist(country);
+			} else {
+				em.merge(country);
+			}
+
+			tx.commit();
+			em.close();
+		}
 	}
 
 	@Override
 	public List<Country> getAllCountries() {
-		// TODO: Implement it
-		return null;
+		EntityManager em = entityManagerFactory.createEntityManager();
+		if (em != null) {
+			List<Country> countryList =
+					em.createQuery("select e from Country e", Country.class)
+							.getResultList();
+			em.close();
+			return countryList;
+		} else {
+			return Collections.emptyList();
+		}
 	}
 
 	@Override
 	public Country getCountryByName(String name) {
-		// TODO: Implement it
-		return null;
+		EntityManager em = entityManagerFactory.createEntityManager();
+		if (em != null) {
+			try {
+				Country country = em.createQuery("select e from Country e where e.name = :name", Country.class)
+						.setParameter("name", name)
+						.getSingleResult();
+				em.close();
+				return country;
+			} catch (NoResultException e) {
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 
 }
