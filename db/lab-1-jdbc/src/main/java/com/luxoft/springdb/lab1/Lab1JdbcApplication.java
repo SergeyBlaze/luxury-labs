18d53add433b583d96edea8af3432b9b7aea550f
Kozlov Sergey <s.kozlov@haulmont.com>
2021-08-26 12:28:35 +0400

    lab1-jdbc migrating to spring boot

---
+++ db/lab-1-jdbc/src/main/java/com/luxoft/springdb/lab1/Lab1JdbcApplication.java
@@ -0,0 +1,13 @@
+package com.luxoft.springdb.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1JdbcApplication.class, args);
+    }
+
+}
